153e500c55c57aa43986fd6b902d6039435bda0f
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-02-07 01:17:10 +0200

    initial commit

---
+++ api/src/test/java/com/bsn/api/BsnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsn.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsnApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
