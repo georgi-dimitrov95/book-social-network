f28cca4efd8d08abc34d901eed5f205c25f2bd4f
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-02-07 05:15:48 +0200

    create Book entity

---
+++ api/src/main/java/com/bsn/api/model/Book.java
@@ -0,0 +1,29 @@
+package com.bsn.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean shareable;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+}
