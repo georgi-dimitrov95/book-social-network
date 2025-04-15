e3ef0aad5c3706237aa23c2e9242cb0a2b589cbe
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-04-16 01:25:24 +0300

    minor changes to validation annotations for request objects

--- api/src/main/java/com/bsn/api/book/BookRequest.java
+++ api/src/main/java/com/bsn/api/book/BookRequest.java
@@ -2,25 +2,27 @@ package com.bsn.api.book;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record BookRequest(
 
         Long id,
 
-        @NotNull
-        @NotEmpty
+        @NotNull(message = "Title is mandatory")
+        @NotEmpty(message = "Title is mandatory")
         String title,
 
-        @NotNull
-        @NotEmpty
+        @NotNull(message = "Author name is mandatory")
+        @NotEmpty(message = "Author name is mandatory")
         String authorName,
 
-        @NotNull
-        @NotEmpty
+        @NotNull(message = "ISBN is mandatory")
+        @NotEmpty(message = "ISBN is mandatory")
         String isbn,
 
-        @NotNull
-        @NotEmpty
+        @NotNull(message = "Synopsis is mandatory")
+        @NotEmpty(message = "Synopsis is mandatory")
+        @Size(min = 20, max = 100, message = "Should be between 20 and 100 characters long")
         String synopsis,
 
         boolean shareable
--- api/src/main/java/com/bsn/api/feedback/FeedbackRequest.java
+++ api/src/main/java/com/bsn/api/feedback/FeedbackRequest.java
@@ -1,20 +1,15 @@
 package com.bsn.api.feedback;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 public record FeedbackRequest(
 
-        @Min(value = 0)
-        @Max(value = 5)
+        @Min(value = 0, message = "Rating should be between 0.0 and 5.0")
+        @Max(value = 5, message = "Rating should be between 0.0 and 5.0")
         Double rating,
 
-        @NotNull
-        @NotEmpty
-        @NotBlank
+        @NotBlank(message = "Comment is mandatory")
+        @Size(min = 8, max = 1000, message = "Comment must be between 8 and 1000 characters long")
         String comment,
 
         @NotNull
