35c0ffe2ee8eac6ee77e54211f0ee89594672bcc
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-04-19 18:01:33 +0300

    add HttpClient provider and import

--- bsn-ui/src/app/app.module.ts
+++ bsn-ui/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
