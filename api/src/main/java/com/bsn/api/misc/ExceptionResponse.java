3991711a2f9b82dd7294e687c5fc4b7ea8c8247e
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-04-23 19:36:16 +0300

    create ExceptionResponseClass

---
+++ api/src/main/java/com/bsn/api/misc/ExceptionResponse.java
@@ -0,0 +1,22 @@
+package com.bsn.api.misc;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+import java.util.Set;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExceptionResponse {
+
+    private String error;
+
+    private Set<String> validationErrors;
+
+    private Map<String, String> errors;
+}
