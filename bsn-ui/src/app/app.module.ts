05329495f39735bda7697ca7818738db23cf970c
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-04-24 02:47:29 +0300

    imports and declarations in module files

--- bsn-ui/src/app/app.module.ts
+++ bsn-ui/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
