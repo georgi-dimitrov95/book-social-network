b70e82ece322d151358178bba880b2960e1628ef
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-05-06 00:48:10 +0300

    create method for handling AccessDeniedException

--- api/src/main/java/com/bsn/api/misc/GlobalExceptionHandler.java
+++ api/src/main/java/com/bsn/api/misc/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.bsn.api.exception.BookNotFoundException;
 import org.apache.logging.log4j.util.InternalException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -64,5 +65,12 @@ public class GlobalExceptionHandler {
         ExceptionResponse response = new ExceptionResponse();
         response.setError("No book found with such an ID");
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ExceptionResponse> handleAccessDeniedException(AccessDeniedException e) {
+        ExceptionResponse response = new ExceptionResponse();
+        response.setError(e.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
     }
 }
