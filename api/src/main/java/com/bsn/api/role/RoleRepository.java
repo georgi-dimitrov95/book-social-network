6b93a41b1c3ed8e7437bd9d01bef04fc32349a0f
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-05-11 23:38:58 +0300

    add @Repository annotation to repositories which were missing it

--- api/src/main/java/com/bsn/api/role/RoleRepository.java
+++ api/src/main/java/com/bsn/api/role/RoleRepository.java
@@ -1,9 +1,11 @@
 package com.bsn.api.role;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
     public Optional<Role> findByName(String name);
