63a440e4e0ea31c26b1a1339d00a1b2326fd73e4
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-05-13 00:02:15 +0300

    implement menu component

---
+++ bsn-ui/src/app/modules/book/components/menu/menu.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  standalone: false,
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss'
+})
+export class MenuComponent {
+
+  constructor(
+  ) {}
+      ngOnInit(): void {
+        const linkColor = document.querySelectorAll('.nav-link');
+        linkColor.forEach(link => {
+          if (window.location.href.endsWith(link.getAttribute('href') || '')) {
+            link.classList.add('active');
+          }
+          link.addEventListener('click', () => {
+            linkColor.forEach(l => l.classList.remove('active'));
+            link.classList.add('active');
+          });
+        });
+      }
+
+  logout(): void {
+    // Implement logout functionality here
+  }
+
+}
