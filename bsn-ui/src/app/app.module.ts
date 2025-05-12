96d8615d036892722a858b06bf1e6390547344b3
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-05-13 00:42:00 +0300

    implement http interceptor which injects the JWT token in each request

--- bsn-ui/src/app/app.module.ts
+++ bsn-ui/src/app/app.module.ts
@@ -8,6 +8,10 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import { HttpTokenInterceptor } from './services/interceptor/http-token.interceptor';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +24,14 @@ import { RegisterComponent } from './pages/register/register.component';
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    HttpClient,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpTokenInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
