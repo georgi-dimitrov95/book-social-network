0a6bc130e1a46a37329b44c0fa54ac1322031297
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-05-14 02:20:04 +0300

    create book-list page

---
+++ bsn-ui/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BookService } from '../../../../services/services/book.service';
+import { PageResponseBookResponse } from '../../../../services/models/page-response-book-response';
+import { BookResponse } from '../../../../services/models/book-response';
+
+@Component({
+  selector: 'app-book-list',
+  standalone: false,
+  templateUrl: './book-list.component.html',
+  styleUrl: './book-list.component.scss'
+})
+export class BookListComponent implements OnInit {
+
+  bookResponse: PageResponseBookResponse = {};
+  page = 0;
+  size = 5;
+  pages: any = [];
+  message = '';
+  level: 'success' | 'error' = 'success';
+
+  constructor(
+    private bookService: BookService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.findAllBooks();
+  }
+
+  private findAllBooks() {
+    this.bookService.findAllBooksOfCurrentUser({
+      page: this.page,
+      size: this.size
+    })
+      .subscribe({
+        next: (books) => {
+          this.bookResponse = books;
+          this.pages = Array(this.bookResponse.totalPages)
+            .fill(0)
+            .map((x, i) => i);
+        }
+      });
+  }
+}
