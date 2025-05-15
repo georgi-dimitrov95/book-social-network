abf92654c175ae39dcbeb78b14d1225e14dee539
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-05-15 21:22:28 +0300

    implement my-books component

---
+++ bsn-ui/src/app/modules/book/pages/my-books/my-books.component.ts
@@ -0,0 +1,84 @@
+import {Component, OnInit} from '@angular/core';
+import {PageResponseBookResponse} from '../../../../services/models/page-response-book-response';
+import {BookService} from '../../../../services/services/book.service';
+import {Router} from '@angular/router';
+import {BookResponse} from '../../../../services/models/book-response';
+
+@Component({
+  selector: 'app-my-books',
+  standalone: false,
+  templateUrl: './my-books.component.html',
+  styleUrl: './my-books.component.scss'
+})
+export class MyBooksComponent implements OnInit {
+
+  bookResponse: PageResponseBookResponse = {};
+  page = 0;
+  size = 3;
+  pages: any = [];
+
+  constructor(
+    private bookService: BookService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.findAllBooks();
+  }
+
+  private findAllBooks() {
+    this.bookService.findAllBooksOfCurrentUser({
+      page: this.page,
+      size: this.size
+    })
+      .subscribe({
+        next: (books) => {
+          this.bookResponse = books;
+          this.pages = Array(this.bookResponse.totalPages)
+            .fill(0)
+            .map((x, i) => i);
+        }
+      });
+  }
+
+  gotToPage(page: number) {
+    this.page = page;
+    this.findAllBooks();
+  }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllBooks();
+  }
+
+  goToLastPage() {
+    this.page = this.bookResponse.totalPages as number - 1;
+    this.findAllBooks();
+  }
+
+  goToNextPage() {
+    this.page++;
+    this.findAllBooks();
+  }
+
+  goToPreviousPage() {
+    this.page--;
+    this.findAllBooks();
+  }
+
+  get isLastPage() {
+    return this.page === this.bookResponse.totalPages as number - 1;
+  }
+
+  archiveBook($event: BookResponse) {
+
+  }
+
+  shareBook($event: BookResponse) {
+
+  }
+
+  editBook($event: BookResponse) {
+
+  }
+}
