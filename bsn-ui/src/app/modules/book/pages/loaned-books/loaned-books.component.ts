fe674ed32217a3b84ddf9bb339c48d1a2a2ad1ce
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-05-21 23:27:25 +0300

    implement feature for displaying the books that a used has loaned to other users

--- bsn-ui/src/app/modules/book/pages/loaned-books/loaned-books.component.ts
+++ bsn-ui/src/app/modules/book/pages/loaned-books/loaned-books.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PageResponseBorrowedBookResponse} from '../../../../services/models/page-response-borrowed-book-response';
+import {BookService} from '../../../../services/services/book.service';
 
 @Component({
   selector: 'app-loaned-books',
@@ -6,6 +8,61 @@ import { Component } from '@angular/core';
   templateUrl: './loaned-books.component.html',
   styleUrl: './loaned-books.component.scss'
 })
-export class LoanedBooksComponent {
+export class LoanedBooksComponent implements OnInit{
 
+  page = 0;
+  size = 5;
+  pages: any = [];
+  loanedBooks: PageResponseBorrowedBookResponse = {};
+
+  constructor(
+    private bookService: BookService,
+  ) {}
+
+  ngOnInit(): void {
+    this.findAllLoanedBooks();
+  }
+
+  private findAllLoanedBooks() {
+    this.bookService.findAllBorrowedBooksFromUser({
+      page: this.page,
+      size: this.size
+    }).subscribe({
+      next: (response) => {
+        this.loanedBooks = response;
+        this.pages = Array(this.loanedBooks.totalPages)
+          .fill(0)
+          .map((x, i) => i);
+      }
+    });
+  }
+
+  goToPage(pageIndex: any) {
+    this.page = pageIndex;
+    this.findAllLoanedBooks();
+  }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllLoanedBooks();
+  }
+
+  goToLastPage() {
+    this.page = this.loanedBooks.totalPages as number - 1;
+    this.findAllLoanedBooks();
+  }
+
+  goToNextPage() {
+    this.page++;
+    this.findAllLoanedBooks();
+  }
+
+  goToPreviousPage() {
+    this.page--;
+    this.findAllLoanedBooks();
+  }
+
+  get isLastPage() {
+    return this.page === this.loanedBooks.totalPages as number - 1;
+  }
 }
