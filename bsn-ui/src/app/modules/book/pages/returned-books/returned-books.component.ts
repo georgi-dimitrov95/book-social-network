2572d8ae5fc31fc4f28556c8beba3bfa6dd081fb
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-05-21 20:34:19 +0300

    implement feature for displaying the books that a user has borrowed and then returned

--- bsn-ui/src/app/modules/book/pages/returned-books/returned-books.component.ts
+++ bsn-ui/src/app/modules/book/pages/returned-books/returned-books.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PageResponseBorrowedBookResponse} from '../../../../services/models/page-response-borrowed-book-response';
+import {BookResponse} from '../../../../services/models/book-response';
+import {FeedbackRequest} from '../../../../services/models/feedback-request';
+import {BookService} from '../../../../services/services/book.service';
+import {FeedbackService} from '../../../../services/services/feedback.service';
+import {BorrowedBookResponse} from '../../../../services/models/borrowed-book-response';
 
 @Component({
   selector: 'app-returned-books',
@@ -6,6 +12,63 @@ import { Component } from '@angular/core';
   templateUrl: './returned-books.component.html',
   styleUrl: './returned-books.component.scss'
 })
-export class ReturnedBooksComponent {
+export class ReturnedBooksComponent implements OnInit {
 
+  page = 0;
+  size = 5;
+  pages: any = [];
+  returnedBooks: PageResponseBorrowedBookResponse = {};
+  message = '';
+  level: 'success' | 'error' = 'success';
+
+  constructor(
+    private bookService: BookService,
+  ) {}
+
+  ngOnInit(): void {
+    this.findAllReturnedBooks();
+  }
+
+  private findAllReturnedBooks() {
+    this.bookService.findAllReturnedBooksByUser({
+      page: this.page,
+      size: this.size
+    }).subscribe({
+      next: (response) => {
+        this.returnedBooks = response;
+        this.pages = Array(this.returnedBooks.totalPages)
+          .fill(0)
+          .map((x, i) => i);
+      }
+    });
+  }
+
+  goToPage(pageIndex: any) {
+    this.page = pageIndex;
+    this.findAllReturnedBooks();
+  }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllReturnedBooks();
+  }
+
+  goToLastPage() {
+    this.page = this.returnedBooks.totalPages as number - 1;
+    this.findAllReturnedBooks();
+  }
+
+  goToNextPage() {
+    this.page++;
+    this.findAllReturnedBooks();
+  }
+
+  goToPreviousPage() {
+    this.page--;
+    this.findAllReturnedBooks();
+  }
+
+  get isLastPage() {
+    return this.page === this.returnedBooks.totalPages as number - 1;
+  }
 }
