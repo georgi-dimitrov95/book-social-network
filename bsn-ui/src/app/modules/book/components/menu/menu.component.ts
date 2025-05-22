569d3f0c4fd4fda371b10f0807e5f59d83d2bcf7
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-05-23 01:33:56 +0300

    implement logout functionality in 'menu' component

--- bsn-ui/src/app/modules/book/components/menu/menu.component.ts
+++ bsn-ui/src/app/modules/book/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -6,25 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
 
   constructor(
+    private router: Router
   ) {}
-      ngOnInit(): void {
-        const linkColor = document.querySelectorAll('.nav-link');
-        linkColor.forEach(link => {
-          if (window.location.href.endsWith(link.getAttribute('href') || '')) {
-            link.classList.add('active');
-          }
-          link.addEventListener('click', () => {
-            linkColor.forEach(l => l.classList.remove('active'));
-            link.classList.add('active');
-          });
-        });
-      }
 
-  logout(): void {
-    // Implement logout functionality here
+  ngOnInit(): void {
+    const linkColor = document.querySelectorAll('.nav-link');
+    linkColor.forEach(link => {
+      if (window.location.href.endsWith(link.getAttribute('href') || '')) {
+        link.classList.add('active');
+      }
+      link.addEventListener('click', () => {
+        linkColor.forEach(l => l.classList.remove('active'));
+        link.classList.add('active');
+      });
+    });
   }
 
+  logout(): void {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
 }
