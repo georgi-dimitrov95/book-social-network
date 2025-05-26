6ba23f5fc360d6fb8be34f1ed0c4bdd39f0a5dfb
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-05-26 21:59:41 +0300

    small visual corrections in books-list component

--- bsn-ui/src/app/modules/book/pages/book-list/book-list.component.ts
+++ bsn-ui/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -14,7 +14,7 @@ export class BookListComponent implements OnInit {
 
   bookResponse: PageResponseBookResponse = {};
   page = 0;
-  size = 3;
+  size = 4;
   pages: any = [];
   message = '';
   success = false;
