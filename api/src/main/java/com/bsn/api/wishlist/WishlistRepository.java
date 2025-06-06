1edda4f78687c632fca7254e36693bf4e97c1ced
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-06-07 01:24:07 +0300

    create WishlistRepository class with a method that checks if a book exists in a user's wishlist

---
+++ api/src/main/java/com/bsn/api/wishlist/WishlistRepository.java
@@ -0,0 +1,24 @@
+package com.bsn.api.wishlist;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class WishlistRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public boolean bookExistsInWishlist(Long userId, Long bookId) {
+       String query = "SELECT COUNT(*) FROM wishlist WHERE user_id = :userId AND book_id = :bookId";
+        Number count = (Number) entityManager.createNativeQuery(query)
+                .setParameter("userId", userId)
+                .setParameter("bookId", bookId)
+                .getSingleResult();
+        return count.intValue() > 0;
+    }
+
+}
