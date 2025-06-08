13c85a3ce02a0acb231fd4b1cb1dacdb383695f6
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-06-09 02:00:45 +0300

    add helper methods in WishlistService

--- api/src/main/java/com/bsn/api/wishlist/WishlistService.java
+++ api/src/main/java/com/bsn/api/wishlist/WishlistService.java
@@ -2,11 +2,20 @@ package com.bsn.api.wishlist;
 
 import com.bsn.api.auth.AuthenticationService;
 import com.bsn.api.book.BookRepository;
+import com.bsn.api.book.BookResponse;
 import com.bsn.api.exception.BookNotFoundException;
+import com.bsn.api.misc.PageResponse;
 import com.bsn.api.user.User;
 import com.bsn.api.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
@@ -20,31 +29,16 @@ public class WishlistService {
 
     private final WishlistRepository wishlistRepository;
 
-    public void addBookToWishlist(Long bookId) {
-        User currentUser = authenticationService.getAuthenticatedUser();
+    private <V, R> PageResponse<R> convertPageToPageResponse(Page<V> page, Function<V, R> mapper) {
+        List<R> responses = page.getContent()
+                .stream()
+                .map(mapper)
+                .toList();
 
-        if (!bookRepository.existsById(bookId)) {
-            throw new BookNotFoundException();
-        }
-
-        if (wishlistRepository.bookExistsInWishlist(currentUser.getId(), bookId)) {
-            throw new IllegalArgumentException("The book already exists in your wishlist");
-        }
-
-        wishlistRepository.addBookToWishlist(currentUser.getId(), bookId);
+        return new PageResponse<>(responses, page);
     }
 
-    public void deleteBookFromWishlist(Long bookId) {
-        User currentUser = authenticationService.getAuthenticatedUser();
-
-        if (!bookRepository.existsById(bookId)) {
-            throw new BookNotFoundException();
-        }
-
-        if (!wishlistRepository.bookExistsInWishlist(currentUser.getId(), bookId)) {
-            throw new IllegalArgumentException("The book does not exist in your wishlist");
-        }
-
-        wishlistRepository.deleteBookFromWishlist(currentUser.getId(), bookId);
+    private User getCurrentUser() {
+        return authenticationService.getAuthenticatedUser();
     }
 }
