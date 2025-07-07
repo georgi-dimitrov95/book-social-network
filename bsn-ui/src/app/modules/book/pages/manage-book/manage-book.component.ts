14b90540bc3a8c45693426fb6b1823213e7d5a7c
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-07-07 03:34:21 +0300

    refactor book cover upload feature in manage-book component

--- bsn-ui/src/app/modules/book/pages/manage-book/manage-book.component.ts
+++ bsn-ui/src/app/modules/book/pages/manage-book/manage-book.component.ts
@@ -15,11 +15,11 @@ export class ManageBookComponent implements OnInit {
   bookRequest: BookRequest = {
     authorName: '',
     isbn: '',
-    synopsis: '',
     title: ''
-  };
+  }
   selectedCover: any;
   displayCover: string | undefined;
+  coverChange: boolean = false;
 
   constructor(
     private bookService: BookService,
@@ -41,6 +41,7 @@ export class ManageBookComponent implements OnInit {
             authorName: book.authorName as string,
             isbn: book.isbn as string,
             synopsis: book.synopsis as string,
+            coverPath: book.coverPath as string,
             shareable: book.shareable
           };
           this.displayCover='data:image/jpg;base64,' + book.cover;
@@ -50,20 +51,13 @@ export class ManageBookComponent implements OnInit {
   }
 
   saveBook() {
-    this.bookService.saveBook({
-      body: this.bookRequest
-    }).subscribe({
-      next: (book) => {
-        this.bookService.uploadBookCoverPicture({
-          'bookId': book.id as number,
-          body: {
-            file: this.selectedCover
-          }
-        }).subscribe({
-          next: () => {
-            this.router.navigate(['/books/my-books']);
-          }
-        });
+    this.bookService.saveBook({body: this.bookRequest})
+      .subscribe({next: (book) => {
+        if (!this.bookRequest.coverPath || this.coverChange) {
+          this.uploadBookCover(book.id as number);
+        } else {
+          this.router.navigate(['/books/my-books']);
+        }
       },
       error: (err) => {
         console.log(err.error);
@@ -72,12 +66,19 @@ export class ManageBookComponent implements OnInit {
     });
   }
 
+  uploadBookCover(bookId: number) {
+    this.bookService.uploadBookCoverPicture({'bookId': bookId, body: {file: this.selectedCover}})
+      .subscribe({next: () => {
+          this.router.navigate(['/books/my-books']);
+        }});
+  }
+
   onFileSelected(event: any) {
     this.selectedCover = event.target.files[0];
     console.log(this.selectedCover);
 
     if (this.selectedCover) {
-
+      this.coverChange = true;
       const reader = new FileReader();
       reader.onload = () => {
         this.displayCover = reader.result as string;
