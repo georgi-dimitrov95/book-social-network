5ba1f1456b167b3629a3b3f0ad2dfe38173207e4
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-07-19 00:24:51 +0300

    update borrow-book-button to work with BookContextService

--- bsn-ui/src/app/modules/shared/components/borrow-book-button/borrow-book-button.component.ts
+++ bsn-ui/src/app/modules/shared/components/borrow-book-button/borrow-book-button.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {BookService} from '../../../../services/services/book.service';
+import {BookContextService} from '../../../../services/context/book-context.service';
 
 @Component({
   selector: 'app-borrow-book-button',
@@ -7,15 +8,27 @@ import {BookService} from '../../../../services/services/book.service';
   templateUrl: './borrow-book-button.component.html',
   styleUrl: './borrow-book-button.component.scss'
 })
-export class BorrowBookButtonComponent {
+export class BorrowBookButtonComponent implements OnInit {
 
   @Input() bookId: any;
 
+  ngOnInit() {
+    if (!this.bookId) {
+      this.bookId = this.bookContext.getBookId();
+    }
+  }
+
   constructor(
-    private bookService: BookService
+    private bookService: BookService,
+    private bookContext: BookContextService
   ) {}
 
   borrowBook() {
+    if (!this.bookId) {
+      console.error('no bookId');
+      return;
+    }
+
     this.bookService.borrowBook({'bookId': this.bookId}).subscribe({
       next: () => {
         console.log('GREAT SUCCESS')
