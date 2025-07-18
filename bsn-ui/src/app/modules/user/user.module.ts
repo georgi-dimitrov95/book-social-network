d65d971289b90a9d4757150550fb5bcd54eb2bcf
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-07-18 23:56:45 +0300

    update user module

--- bsn-ui/src/app/modules/user/user.module.ts
+++ bsn-ui/src/app/modules/user/user.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserCardComponent } from './components/user-card/user-card.component';
 import { UserCarouselComponent } from './components/user-carousel/user-carousel.component';
-
+import {SharedModule} from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [
-    UserCardComponent,
-    UserCarouselComponent
-  ],
+    declarations: [
+        UserCardComponent,
+        UserCarouselComponent
+    ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
   ],
   exports: [
     UserCardComponent,
