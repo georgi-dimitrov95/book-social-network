4447a8168d1c56a508230703bb756c201302dabc
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-07-24 23:30:08 +0300

    create borrow-service

---
+++ bsn-ui/src/app/services/custom/borrow-service.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {BookService} from '../services/book.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BorrowServiceService {
+
+  constructor(
+    private bookService: BookService
+  ) {}
+
+  borrowBook(bookId: number) {
+    this.bookService.borrowBook({ bookId }).subscribe({
+      next: () => {console.log('GREAT SUCCESS')},
+      error: (err) => {console.log(err);}
+    });
+  }
+
+  borrowBookByTitleFromUser(title: string, userId: number) {
+    this.bookService.borrowBookByTitleFromUser({ title, userId }).subscribe({
+      next: () => {console.log('GREAT SUCCESS')},
+      error: (err) => {console.log(err);}
+    })
+  }
+}
