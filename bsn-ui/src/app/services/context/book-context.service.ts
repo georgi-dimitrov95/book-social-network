bd101b1eddd315cea9d7fc1b13fb749f5a275dad
Georgi Dimitrov <georgi.dimov.dimitrov95@gmail.com>
2025-07-26 03:04:13 +0300

    extend BooKContextService to also store the title of a book

--- bsn-ui/src/app/services/context/book-context.service.ts
+++ bsn-ui/src/app/services/context/book-context.service.ts
@@ -8,11 +8,22 @@ export class BookContextService {
   private bookIdSubject = new BehaviorSubject<number | null>(null);
   bookId$: Observable<number | null> = this.bookIdSubject.asObservable();
 
+  private bookTitleSubject = new BehaviorSubject<string | null>(null);
+  bookTitle$: Observable<string | null> = this.bookTitleSubject.asObservable();
+
   setBookId(bookId: number) {
     this.bookIdSubject.next(bookId);
   }
 
   getBookId(): number | null {
     return this.bookIdSubject.value;
+  }
+
+  setBookTitle(bookTitle: string) {
+    this.bookTitleSubject.next(bookTitle);
+  }
+
+  getBookTitle() {
+    return this.bookTitleSubject.value;
   }
 }
